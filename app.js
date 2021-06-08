ca02710bce7a299ba049ca8943b11534b8eebccd
oljapriya <poljako@gmail.com>
2021-06-07 21:39:51 -0400

    fixed

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const Manager = require("./lib/Manager.js");
-const Engineer = require("./lib/Engineer.js");
-const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-// const path = require("path");
+const path = require("path");
 const fs = require('fs');
 
 const output_dir = path.resolve(__dirname, "output");
 const outputPath = path.join(output_dir, "team.html");
 
 const render = require("./lib/htmlRenderer");
-// const { POINT_CONVERSION_COMPRESSED } = require("constants");
+const { POINT_CONVERSION_COMPRESSED } = require("constants");
 
 const teamMembers = [];
 // question arrays
@@ -37,7 +37,7 @@ const managerQuestions = [
   },
 ];
 
-  // intern
+  // engineer
   const engineerQuestions = [
     {
       type: 'input',
@@ -88,22 +88,22 @@ const managerQuestions = [
   const teamAdd = 
   {
     type: "list",
-    message:"What type of team member would you like to add",
+    message:"What type of team member would you like to add?",
     name: "team",
-    choices: ["Intern", "Intern", "None"]
+    choices: ["Engineer", "Intern", "None"]
   }
   //manager
   function createManager() {
     inquirer.prompt(managerQuestions).then(response => {
       const newManager = new Manager (response.name, response.id, response.email, response.office)
-      teamMember.push(newManager)
+      teamMembers.push(newManager)
       createTeam()
     })
   }
   function createEngineer() {
     inquirer.prompt(engineerQuestions).then(response => {
       const newEngineer = new Engineer (response.name, response.id, response.email, response.github)
-      teamMember.push(newEngineer)
+      teamMembers.push(newEngineer)
       createTeam()
     })
   }
@@ -111,7 +111,7 @@ const managerQuestions = [
   function createIntern() {
     inquirer.prompt(internQuestions).then(response => {
       const newIntern = new Intern (response.name, response.id, response.email, response.school)
-      teamMember.push(newIntern)
+      teamMembers.push(newIntern)
       createTeam()
     })
   };
@@ -125,11 +125,11 @@ const managerQuestions = [
         createIntern();
       } else if (response.team === "None") {
         const finalHTML = render(teamMembers)
-        fs.writeFile("./output/team.html", finalHTML, function(err) {
+        fs.writeFile(outputPath, finalHTML, function(err) {
           if (err) {
             return console.log(err);
           }
-          console.log("File.written!");
+          console.log("File written!");
         });
       }
     })
